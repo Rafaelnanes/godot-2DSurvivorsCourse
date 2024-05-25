e7b5ed786acf7c2b2fad6b5b292420e25dc4bd53
Rafael Nanes <rafaelbraga_22@hotmail.com>
2024-05-25 16:16:50 +0100

    added feature to kill enemies

---
+++ scenes/basic_enemy/basic_enemy.gd
@@ -0,0 +1,28 @@
+extends CharacterBody2D
+
+
+const MAX_SPEED = 75
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$Area2D.area_entered.connect(func(area2D: Area2D):
+		queue_free()
+	)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var player_direction = get_direction_to_player() 
+	velocity = player_direction * MAX_SPEED
+	move_and_slide()
+
+
+func get_direction_to_player() -> Vector2:
+	var player_node = get_tree().get_first_node_in_group("player") as Node2D
+	if player_node == null:
+		return Vector2.ZERO
+	return (player_node.global_position - global_position).normalized()
+
+
+func _on_area_2d_area_entered(area):
+	pass # Replace with function body.
